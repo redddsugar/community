34a85f180ccf50ed45d7594271c1b8b5cb6eca93
sansa <1741056830@qq.com>
2020-04-21 21:41:34 +0800

    新增注册模块

--- src/main/java/com/ln/community/controller/HomeController.java
+++ src/main/java/com/ln/community/controller/HomeController.java
@@ -8,6 +8,7 @@ import com.ln.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,7 +25,7 @@ public class HomeController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    @GetMapping("/index")
     public String getIndexPage(Model model, Page page) {
         // 方法调用前,SpringMVC会自动实例化Model和Page,并将Page注入Model.
         // 所以,在thymeleaf中可以直接访问Page对象中的数据.
--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,3 +1,4 @@
+CONTEXT_PATH = "/community"
 window.alert = function(message) {
 	if(!$(".alert-box").length) {
 		$("body").append(
